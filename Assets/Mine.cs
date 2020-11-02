08bf3235fe744014258dce159168383c637dbc6f
izzy <44009748+izzyphobe@users.noreply.github.com>
2020-11-02 14:01:33 -0600

    mine/spawner/map placement basics

---
+++ Assets/Mine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+    Transform trans;
+    Faction faction;
+    Player player;
+    Renderer render;
+    Map map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = GameObject.Find("Map").GetComponent<Map>();
+        render = gameObject.GetComponent<Renderer>();
+        trans = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public (int row, int col) getLocation(){
+        return map.tilePosition(render);
+    }
+
+    public (int x, int y) getSize(){
+        return map.tileScale(render);
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private bool isOffense;
+    private int spawnDelay;
+    private int maxHealth;
+    private int currentHealth;
+    private int resourceDrain;
+    public GameObject unitPrefab;
+    Transform trans;
+    Faction faction;
+    Player player;
+    Renderer render;
+    Map map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = GameObject.Find("Map").GetComponent<Map>();
+        render = gameObject.GetComponent<Renderer>();
+        trans = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setVals(bool off, int spawn){
+        isOffense = off;
+        spawnDelay = spawn;
+    }
+
+    public (int row, int col) getLocation(){
+        return map.tilePosition(render);
+    }
+
+    public (int x, int y) getSize(){
+        return map.tileScale(render);
+    }
+}
